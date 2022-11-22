9c0116ab03e71b2e589c696484280ff72a4a01c5
Renato Wéssmėr <rewesa@outlook.com.br>
2022-11-21 23:20:15 -0300

    Create Cliente.java

---
+++ AgenciaNyssa/src/main/java/model/Cliente.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Cliente {
+	
+	private int id;
+	private String nome;
+	private String rg;
+	private String cpf;
+	private String telefone;
+	private String email;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + ", rg=" + rg + ", cpf=" + cpf + ", telefone=" + telefone
+				+ ", email=" + email + "]";
+	}
+	
+}
